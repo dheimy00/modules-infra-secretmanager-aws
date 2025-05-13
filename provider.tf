565343b94a7e8da5b051664cfc2efbcb8adf3a5d
Dheimy <jheimyeng@hotmail.com>
2025-05-13 10:24:40 -0300

    fix: ajust infra

--- provider.tf
+++ provider.tf
@@ -7,13 +7,3 @@ terraform {
   }
   required_version = ">= 1.0.0"
 }
-
-provider "aws" {
-  region = var.aws_region
-}
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-} 
--- variables.tf
+++ variables.tf
@@ -18,12 +18,6 @@ variable "tags" {
   default     = {}
 }
 
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "secret_name" {
   description = "The name of the secret"
   type        = string
